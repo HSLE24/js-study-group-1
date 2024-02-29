2091ba1e5f9fdad875ad45d7d63cb5ae0e23d735
insovu9 <158449906+insovu9@users.noreply.github.com>
2024-02-29 17:36:13 +0900

    2th commit
    추천 레시피 이미지 호출 완료

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_
     console.error('API 요청 중 오류가 발생했습니다:', error);
   });
 
-// 비슷한 레시피 이미지 가져오기
 fetch(`https://api.spoonacular.com/recipes/${recipeId}/similar?apiKey=${API_KEY}`)
   .then(response => {
     if (!response.ok) {
@@ -34,9 +33,22 @@ fetch(`https://api.spoonacular.com/recipes/${recipeId}/similar?apiKey=${API_KEY}
   .then(data => {
     // 추천 레시피 이미지 추가
     data.slice(0, 2).forEach((recipe, index) => {
-      recommendationImages[index].src = recipe.image;
-      recommendationImages[index].alt = recipe.title;
-      console.log(data)
+      const recommendationImageElement = recommendationImages[index]; // 이미지를 보여줄 요소
+      fetch(`https://spoonacular.com/recipeImages/${recipe.id}-556x370.jpg`) // 이미지의 유형을 알고 있을 경우에는 확장자를 명시적으로 지정합니다.
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('이미지를 불러오는 중 문제가 발생했습니다.');
+          }
+          return response.blob();
+        })
+        .then(blob => {
+          const imageUrl = URL.createObjectURL(blob);
+          recommendationImageElement.src = imageUrl;
+          recommendationImageElement.alt = recipe.title;
+        })
+        .catch(error => {
+          console.error('이미지를 불러오는 중 오류가 발생했습니다:', error);
+        });
     });
   })
   .catch(error => {
