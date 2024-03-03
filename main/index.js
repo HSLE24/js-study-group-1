0f2ac10000ee0a66f1c5fd980d1875844f0e8971
Hwiho Lee <lhh323@gmail.com>
2024-03-03 14:46:23 +0900

    3/3

--- main/index.js
+++ main/index.js
@@ -60,7 +60,7 @@ const getInfo2=async(id)=>{
     const infoData = await response.json();
     recipesList = infoData;
     console.log("six", infoData);
-    recipeRender(infoData)
+    recipeRender2(infoData)
 
     const instructionsUrl = new URL(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${API_KEY}`);
     const instructionsResponse = await fetch(instructionsUrl);
@@ -135,6 +135,49 @@ const recipeRender = (infoData, instructionsData) => {
     </div>`;
 
     document.getElementById('recipe-board').innerHTML = recipeHTML;
+}
+
+const recipeRender2 = (infoData, instructionsData) => {
+    const { id, title, image, summary, winePairing, extendedIngredients } = infoData;
+    const pairingText = winePairing ? winePairing.pairingText : "N/A";
+
+    let ingredientsList = "<ul>";
+    extendedIngredients.forEach(ingredient => {
+        ingredientsList += `<li>${ingredient.original}</li>`;
+    });
+    ingredientsList += "</ul>";
+
+    let instructionsList = "";
+    if (instructionsData && instructionsData.length > 0) {
+        instructionsList = "<ol>";
+        instructionsData[0].steps.forEach(step => {
+            instructionsList += `<li>${step.step}</li>`;
+        });
+        instructionsList += "</ol>";
+    } else {
+        instructionsList = "Instructions not available.";
+    }
+
+    let recipeHTML = `<div class="row info-area">
+        <div class="col-lg-12" name="tag2">
+            <p style="font-size:30px;" class="text-center">${title}</p>
+            
+            <div><img src="${image}" style="width:100%;"></div>
+            <div class="mt-5"><b>Summary</b> : ${summary}</div>
+            <div class="mt-3 text-center">
+                <button class="toggle-button" onclick="toggleIngredients(${id})">Ingredients</button>
+                <button class="toggle-button" onclick="toggleInstructions(${id})">Instructions</button>
+                <button class="toggle-button" onclick="toggleWinePairing(${id})">Wine Pairing</button>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div id="ingredients-${id}" style="display: none;"><b>Ingredients</b>: ${ingredientsList}</div>
+            <div id="instructions-${id}" style="display: none;"><b>Instructions</b>: ${instructionsList}</div>
+            <div id="wine-pairing-${id}" style="display: none;"><b>Wine Pairing</b>: ${pairingText}</div>
+        </div>
+    </div>`;
+
+    document.getElementById('search-board').innerHTML = recipeHTML;
 }
 
 // Toggle functions for each section
